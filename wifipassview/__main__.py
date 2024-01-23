a730a03f7ed9814c5322c1aeba7f4c5068c383c1
frosty <inthishouseofcards@gmail.com>
2024-01-22 20:46:31 -0500

    move some stuff around

--- wifipassview/__main__.py
+++ wifipassview/__main__.py
@@ -1,15 +1,23 @@
 import click
 
 import wifipassview.wifipassview as wpv
+from wifipassview import __version__
 
 
-@click.group()
+@click.group(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.version_option(__version__)
 def cli() -> None:
+    """Utility to view Wi-Fi profiles and their passwords.
+
+    Copyright (c) 2024 frosty.
+    """
     pass
 
 
-@cli.command(name="list", help="List all Wi-Fi profiles on the system.")
+@cli.command(name="list")
 def list_command() -> None:
+    """List all Wi-Fi profiles on the system."""
+
     if profiles := wpv.get_profiles_list():
         for profile in profiles:
             click.echo(f" - {profile}")
@@ -17,9 +25,11 @@ def list_command() -> None:
         click.echo("No profiles found.")
 
 
-@cli.command(name="view", help="View the password for a profile.")
+@cli.command(name="view")
 @click.argument("profile", type=str)
 def view_command(profile: str) -> None:
+    """View the password for a profile."""
+
     if password := wpv.get_password(profile):
         click.echo(password)
     else:
